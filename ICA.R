ca8b83d524be5f08d1930e9efb44c42d68394b53
mlouarn <marynn.kokoelma@gmail.com>
2026-02-13 14:24:33 +0100

    update

--- ICA.R
+++ ICA.R
@@ -1,4 +1,6 @@
 ###ICA
+int.combined = readRDS(file = "PYMT\ scRNAseq/boissonas_2022_tam-breast_GSE184096/int_combined.rds")
+
 int.combined <- RunICA(int.combined)
 
 genes = signatures_mouse$Signature_gene_mouse.style_symbol
@@ -10,7 +12,36 @@ df_s$id <- NULL
 sum = colSums(df_s)
 sum = rank(-sum)
 sum = sum[sum<6]
-rownames(as.matrix(sum))
+list_ICA = rownames(as.matrix(sum))
+
+
+df_ICA =select(as.data.frame(int.combined@reductions$ica@cell.embeddings),all_of(list_ICA))
+df_pca = as.data.frame(int.combined@reductions$pca@cell.embeddings)
+
+new_dim = merge(df_ICA, df_pca[,1:10], by=0)
+rownames(new_dim) = new_dim$Row.names
+new_dim$Row.names <- NULL
+colnames(new_dim) <- paste0("ICAnPCA_", 1:15)
+
+int.combined[["ICAnPCA"]] <- CreateDimReducObject(embeddings =  as.matrix(new_dim), key = "ICAnPCA_", assay = DefaultAssay(int.combined)) #custom dimred
+DimPlot(int.combined, reduction = "ICAnPCA", pt.size = 0.5)
+
+int.combined <- FindNeighbors(int.combined, reduction = "ICAnPCA")
+int.combined <- FindClusters(int.combined, resolution = 1)
+int.combined <- RunUMAP(int.combined,dims = 1:15)
+DimPlot(int.combined, reduction = "umap", label = TRUE)
+
+FeaturePlot(int.combined,features=unique(signatures_mouse$LEVEL_1))& 
+  scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "Spectral")))
+VlnPlot(int.combined,features=unique(signatures_mouse$LEVEL_1), group.by='seurat_clusters')
+
+FeaturePlot(int.combined,features=unique(signatures_mouse$LEVEL_2))& 
+  scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "Spectral")))
+VlnPlot(int.combined,features=unique(signatures_mouse$LEVEL_2), group.by='seurat_clusters')
+
+FeaturePlot(int.combined,features=unique(signatures_mouse$LEVEL_3))& 
+  scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "Spectral")))
+VlnPlot(int.combined,features=unique(signatures_mouse$LEVEL_3), group.by='seurat_clusters')
 
 #2nd option
 df$Signature = with(df, ifelse(rownames(df) %in% genes, 'In_signature', 'Not'))
