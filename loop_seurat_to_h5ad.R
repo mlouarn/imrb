0140e7b29a6c5562bfc2d83be19334d10fef0b0a
naourg <199085767+NaourG@users.noreply.github.com>
2026-02-25 17:34:33 +0100

    ignore files that are already h5ad

--- loop_seurat_to_h5ad.R
+++ loop_seurat_to_h5ad.R
@@ -4,10 +4,13 @@ library(stringr)
 
 list_rds = list.files(pattern = "\\.rds$")
 for(filename_rds in list_rds){
+  if(file.exists(str_replace(filename_rds, "rds$", "h5ad"))){
+    next()
+  }
   print(filename_rds)
   seurat_obj = readRDS(filename_rds)
   adata_obj = as_AnnData(seurat_obj)
-  write_h5ad(adata_obj, str_replace(filename_rds, 
+  write_h5ad(adata_obj, str_replace(filename_rds,
                                     pattern = "\\.rds$",
                                     replacement = ".h5ad"))
   print("done")
